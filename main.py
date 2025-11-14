b36b617f7594934c1d4598f272e6e17b8458d964
ronak00244-cmyk <ronak00244@gmail.com>
2025-11-14 12:34:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,588 @@
+# print("hello world")
+# print("five greater are two")
+
+# x=5
+# y="hello"
+# print("x+y")
+# x=5
+# y=3
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print("this is python")
+
+
+# x=5
+# y="ronak"
+# print("x")
+# print(y)
+
+
+# x=4.5
+# print((x))
+
+# x=5
+# y=4
+# print(bool(x+y))
+# print(bool(x))
+# x=""
+# print(bool(x))
+
+
+# x="e"
+# print(bool(x))
+
+
+# my_var=40
+# print(my_var)
+
+
+# x,y,z="30","40","50"
+# x="30","40","50"
+
+
+# print(x)
+# x=y=z="apple"
+# print(x)
+
+
+
+# fruits=["applr","banana","cherry"]
+# x,y,z=fruits
+# x=y=z=fruits
+# print(x)
+# print(type(z))
+
+
+# fruits=[2,4,7,8,34,50]
+# print(fruits)
+
+
+
+# print(type(fruits))
+
+
+# x="this is python"
+# print(x)
+
+
+# x="python"
+# y="is"
+# z="good"
+# print(x,y,z)
+# print(x+y+z)
+
+
+# x="good"
+
+
+# #global var
+
+# def my_function():
+#     print("python is" + x)
+#     my_function()
+
+
+#     def my_function():
+#         global x
+#         x="good"
+#         my_function()
+#         print("python is" + x)
+
+
+
+#         #data type
+
+
+#         x=5
+#         print(type(x))
+
+#         x="name"
+#         print(type(x))
+
+
+#         x="5.6"
+#         print(type(x))
+
+#         x=5.6
+#         print(type(x))
+
+#         x=1j
+#         print(type(x))
+#         x=10
+#         y=20
+
+#         print(x+y)
+#         print(x-y)
+#         print(x*y)
+#         print(x/y)
+#         print(x%y)
+#         print(x**y)
+#         print(x//y)
+
+
+# x="good"
+# print(range)
+
+
+
+# #list
+
+
+# my_list=["apple","banana"]
+# print(type(my_list))
+
+# print(my_list)
+
+
+# my_list=["apple","banana","cherry"]
+# print(my_list[1])
+
+
+# print(my_list[-1])
+
+
+# my_list=["apple","banana","cherry","sjc","bkdcsgkx","vbhsvc"]
+# print(my_list[2:5])
+
+# print(my_list[-5:-2])
+
+# print(my_list[-4:0])
+
+# print(my_list[-4])
+
+# my_list=["apple","banana","cherry","sjc","bkdcsgkx","vbhsvc"]
+# my_list[1]="mango"
+# print(my_list)
+
+
+
+
+
+
+# my__list=["apple","banana", 1,3,5,7]
+
+# my__list[2]
+
+
+# my__list=["apple","banana", 1,3,5,7]
+
+# my_list=[1,2,3,4,5,6]
+
+# my_list.insert(2,"svchm")
+# my_list.append("ksbckj")
+
+# print(my_list)
+
+
+
+# list1=[1,2,3]
+# list2=[4,5,6]
+
+# list1.extend(list2)
+
+# print(list1)
+
+
+
+
+# #remove
+
+# list1=[1,2,3,"apple",4,5,6]
+
+# #list1.remove("apple")
+# list1.pop(3)
+# print(list1)
+
+
+# list1=[1,2,3,"apple",4,5,6]
+
+# #list1.remove("apple")
+# list1.pop()
+# print(list1)
+
+# list=[1,2,3,4,5,6]
+
+# list.insert(2,"apple")
+# print(my_list)
+
+# list1.append("banana")
+# print(my_list)
+
+# list1.remove("apple")
+# print(list1)
+
+# list1.pop()
+# print(list1)
+
+# list1.clear()
+# print(list1)
+
+
+
+# list1=[]
+
+# list1.append(1)
+# print(list1)
+
+# list1[0]="hello"
+# print(list1)
+
+
+# #loopslists
+
+# list1=[1,2,3,4,5,6]
+# for x in list1:
+#     print(x)
+
+
+#     #sort
+#     list=[2,5,1,4,8]
+
+#     list.sort()
+#     print(list)
+
+#     list=[2,5,1,4,8]
+
+#     list.sort(reverse=True)
+#     print(list)
+
+
+#     text="hello python"
+
+#     print(text.upper())
+#     print(text.lower())
+#     print(text.strip())
+#     print(text.replace("python","world"))
+#     print(text.split())
+
+
+#     x=5
+#     y="bdcbk"
+    
+
+#     #format string
+
+#     Name="bob"
+#     age=20
+#     # print(x+y)
+
+#     x="bob"
+#     y=20
+#     print(f"my name is {x} and my age is {y}")
+
+
+#     x=5
+#     y=10
+
+#     print(x==y)
+#     print(x!=y)
+#     print(x>=y)
+#     print(x<=y)
+#     print(x>y)
+#     print(x<y)
+
+#     x=8
+#     y=x
+#     z=10
+#     print(y)
+#     print(z is y)
+#     print(x is not y)
+
+#     colors={"red","green","blue"}
+#     print(colors)
+
+#     colors.add("yellow")
+#     print(colors)
+
+#     colors.remove("green")
+#     print(colors)
+
+#     for colors in colors:
+#         print(colors)
+
+
+# student={"name":"alice","age":20,"grade":"A"}
+# print(student)
+
+# print(student["name"]) #Alice
+# print(student.get("age")) #20
+
+# student["age"]=21
+# student["city"]="delhi"
+# print(student)
+
+# student.pop("grade")
+# del student["city"]
+# print(student)
+
+# for key, value in student.items():
+#     print(key,":",value)
+
+# students={
+#     "name":"ronak",
+#           "age":30,
+#           "grade":"A"
+#           }
+
+
+# print(student)
+
+
+
+
+# #if else
+
+# age=20
+
+# if age>=18:
+#     print("you are adult")
+# else:
+#     print("you are not adult")
+
+
+#     #if else 
+
+#     age=18
+
+#     if age>=18:
+#         print("you are adult")
+
+#     elif age==18:
+#         print("you are teen")
+
+#     elif age==17:
+#         print("you are 17")
+
+#     elif age==16:
+#         print("you are 16")
+
+#     else:
+#         print("you are not adult")
+
+
+
+# x=15 
+
+# if x>10:
+#     print("x is greater than 10")
+
+#     if x>20:
+#         print("x is greater than 20")
+#     else:
+#         print("x is not greater than 20") 
+         
+#     x=10242783
+    
+# if x%2==0:
+#     print("x is an even no")
+# else:
+#     print("x is an odd no")
+
+#     fruits=["apple","banana","cherry"]
+# print(fruits)
+
+# word="python"
+
+# for x in word:
+#     print(x)
+
+# for i in range(5):
+#        print(i)
+
+# for i in range(1,10,2):
+#         print(i)
+
+# for i in range(7,14,3):
+#          print(i)
+
+    
+# for i in range(1,4):
+     
+#  for j in range(1,3):
+#   print(f"i={i},j={j}")
+
+# for i in range(1,6):
+#     if i==4:
+#         break
+#     print(i)
+
+# for i in range(1,6):
+#     if i==3:
+#         break
+#     print(i)
+
+#     for i in range(1,4):
+#      print(i) 
+#     else:
+#         print("loop is finished") 
+
+# for i in range(1,20):
+#       print(i)
+
+# for i in range(1,30):
+#     if i%2==0:
+#         print(i)
+
+#         colors=["blue","black","yellow","red"]
+#         for colors in colors:
+#             print(colors)
+
+# #Functions
+
+# def greet():
+#     print("hello,python!")
+
+# greet() #calling the function
+
+# #functions
+
+# def greet(name):
+#     print(f"hello,{name}")
+
+# greet("alice")
+# greet("bob")
+
+
+# #function with return
+
+# def add(a,b):
+#     return a+b
+
+# result=add(5,3)
+# print(result) #8
+
+# def add(a,b):
+#     return a/b
+
+# result=add(5,3)
+# print(result) #8
+
+# def add(a,b):
+#     return a-b
+
+# result=add(5,3)
+# print(result) #8
+
+# def add(a,b):
+#     return a*b
+
+# result=add(5,3)
+# print(result) #8
+
+
+# def greet(name="student"):
+#     print(f"hello,{name}")
+# greet()
+# greet("alice")
+
+# x=10 #global
+
+# def my_func():
+#     y=5 #local 
+#     print("inside",x,y)
+
+# my_func()
+# print("outside:",x)
+# #print(y)->Error,y is local
+
+# def greet():
+#     print("Hello")
+# greet()
+
+
+# def add(a,b):
+#     return a+b
+
+# result=add(6,2)
+# print(result) 
+
+
+# #class
+# class car:
+#    def __init__(self,brand,color):
+#     self.brand=brand
+#     self.color=color
+#    def drive(self):
+#     print(f"{self.color}{self.brand}is driver🚗")
+
+
+# car1=car("bmw","black")
+# car2=car("Tesla","white")
+
+# car1.drive()
+# car2.drive()
+
+
+
+#array
+
+# import array
+#create an array of integers
+
+# numaber=array.array('i',[1,2,3,4,5])
+# print(number)
+
+
+# from numpy import random
+
+# x=random.randint(100)
+# print(x)
+
+# x=random.rand()
+# print(x)
+
+# [54]
+# [1,2,3,4,5]
+
+# [[[1,2,3,],
+# [4,5,6]],
+#[7,8,9],
+
+# x=random.randint(100,size=(5))
+# print(x)
+# x=random.randint(100,size=(3,5))
+# print(x)
+
+
+# x=random.randint(100,size=(4,5,7))
+# print(x)
+
+
+# x=random.choice([4,5,6],size=(2,3,5))
+# print(x)
+
+### python -m pip install pandas
+
+##pandas
+
+# import pandas as pd 
+
+# data=[10,20,30,40]
+
+# s=pd.Series(data)
+
+# print(s) 
+# import pandas as pd
+# data={
+#     "Name":["Alice","Bob","Charlie","David"],
+#     "Age":[24,27,22,32],
+#     "City":["Delhi","Mumbai","Chennai","Kolkata"]
+# }
+# df=pd.DataFrame(data)
+# print(df) 
+
+# import numpy as np 
+
+# arr=np.array([1,2,3,4,5])
+# s=pd.Series(arr)
+
+# print(s)
+
+
+# data={"math":90,"science":85,"English":78}
+
+# s=pd.Series(data)
+
+# print(s)
